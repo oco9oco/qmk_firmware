e2d1bc230c7e57ba0377d62f51bfe83f6cc12a1b
Sungsoo Ryu <40929552+oco9oco@users.noreply.github.com>
2022-03-03 13:29:43 +0900

    mousekey

--- users/oco9oco/config.h
+++ users/oco9oco/config.h
@@ -18,10 +18,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define PERMISSIVE_HOLD_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
-#define MOUSEKEY_INTERVAL 1
+#define MOUSEKEY_INTERVAL 16
 #define MOUSEKEY_DELAY 0
-#define MOUSEKEY_MAX_SPEED 3
-#define MOUSEKEY_TIME_TO_MAX 29
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_TIME_TO_MAX 60
 #define MOUSEKEY_WHEEL_MAX_SPEED 4
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 18
 
