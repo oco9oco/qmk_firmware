3be12630fe6e85e0cded570029dd62fe658c39f4
Sungsoo Ryu <40929552+oco9oco@users.noreply.github.com>
2022-03-04 07:35:09 +0900

    mousekey_kinetic_test

--- users/oco9oco/config.h
+++ users/oco9oco/config.h
@@ -17,15 +17,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
 //#define PERMISSIVE_HOLD_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+//MOUSEKEY
+
+#define MK_KINETIC_SPEED
 
 #define MOUSEKEY_INTERVAL 16
 #define MOUSEKEY_DELAY 0
-#define MOUSEKEY_MAX_SPEED 7
-#define MOUSEKEY_TIME_TO_MAX 60
-#define MOUSEKEY_WHEEL_MAX_SPEED 4
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 18
 
-#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#ifdef  MK_KINETIC_SPEED
+    #define MOUSEKEY_INITIAL_SPEED 5
+    #define MOUSEKEY_BASE_SPEED 200
+    #define MOUSEKEY_DECELERATED_SPEED 100
+    #define MOUSEKEY_ACCELERATED_SPEED 500
+    #define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS 8
+    #define MOUSEKEY_WHEEL_BASE_MOVEMENTS 16
+    #define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 24
+    #define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS 12
+#else
+    #define MOUSEKEY_MAX_SPEED 7
+    #define MOUSEKEY_TIME_TO_MAX 60
+    #define MOUSEKEY_WHEEL_MAX_SPEED 4
+    #define MOUSEKEY_WHEEL_TIME_TO_MAX 18
+#endif
+
+#define ONESHOT_TAP_TOGGLE 1  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000
 
 #define COMBO_TERM 40
