d7c1252dc41a28c9f0329433fd088bfe62e29530
Sungsoo Ryu <40929552+oco9oco@users.noreply.github.com>
2022-03-03 13:23:21 +0900

    KEYMAP UPDATE

--- keyboards/keycapsss/kimiko/keymaps/oco9oco/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/oco9oco/keymap.c
@@ -93,11 +93,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ONEHAND] = LAYOUT_kimiko_wrapper(
 // |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
     TO_BASE, CTRL_F1, KC_F2,   KC_ENT,   ALT_F4, KC_F5,                              _______, _______, _______, _______, _______, _______,
-    GUI_RGHT,KC_LGUI, KC_BTN2, KC_UP,   KC_BTN1, SCRNSHOT,                           BRKT,    KC_7,    KC_8,    KC_9,    _______, _______, 
-    KC_LALT, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, PDF_HL,                             PRNS,    KC_4,    KC_5,    KC_6,    KC_LGUI, KC_LSFT, 
-    KC_LSFT, UNDO,    CUT,     COPY,    PASTE,   BOLDFACE, KC_ENT,          XXXXXXX, _______, KC_1,    KC_2,    KC_3,    KC_LCTL, KC_LALT, 
+    GUI_RGHT,KC_LGUI, KC_BTN2, KC_UP,   KC_BTN1, SCRNSHOT,                           BRKT,    KC_WH_D, KC_MS_U, KC_WH_U, _______, _______,
+    KC_LALT, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, PDF_HL,                             PRNS,    KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,
+    KC_LSFT, UNDO,    CUT,     COPY,    PASTE,   BOLDFACE, KC_ENT,          XXXXXXX, _______, _______, _______, KC_LCTL, _______, _______,
 // |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-                      KC_BSPC, KC_LALT, KC_SPC,  KC_BTN3, _______,          _______, LT_NAV_0,_______, _______, _______
+                      KC_BSPC, KC_LALT, KC_SPC,  KC_BTN3, _______,          _______, _______, _______, _______, _______
 ),
 
 [_SETTINGS] = LAYOUT(
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
-   RGB_TOG, A(KC_S), C(KC_W), XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+   RGB_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 //|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
                      KC_TRNS, _______, _______, _______, _______,          _______, _______, _______, _______, _______ 
 )
