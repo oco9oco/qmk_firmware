c4558e30404aefe002aeb99408ed03fce8edb4d1
Sungsoo Ryu <40929552+oco9oco@users.noreply.github.com>
2022-04-28 14:36:30 +0900

    COMMIT

--- keyboards/keycapsss/kimiko/keymaps/oco9oco/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/oco9oco/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, CTRL_F1, KC_F2,   KC_F3,   ALT_F4,  KC_F5,                              KC_LT,   KC_LPRN, KC_GRV,  KC_RPRN, KC_GT, KC_UNDS,
     _______, KC_B,    KC_C,    KC_E,    KC_D,    KC_F6,                              KC_LBRC, _________NUM_789_________, KC_RBRC, KC_PLUS,
     _______, IPC(A),  IPC(S),  IPC_MIN, IPC(F),  KC_SLSH,                            KC_GRV,  _________NUM_456_________, KC_COLN, KC_DQUO,
-    _______, KC_LSFT, BRKT,    IPC(C),  KC_EQL,  BOLDFACE,XXXXXXX,          XXXXXXX, XXXXXXX, _________NUM_123_________, _______, KC_PIPE,
+    _______, KC_LSFT, BRKT,    IPC(C),  KC_EQL,  BOLDFACE, XXXXXXX,          XXXXXXX, XXXXXXX, _________NUM_123_________, _______, KC_PIPE,
 // |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-                      _______, _______, _______, _______, _______,          _______, KC_0, _______, KC_LPRN, KC_RPRN
+                      _______, _______, _______, _______, _______,          _______, LT(_NAV, KC_0), _______, KC_LPRN, KC_RPRN
 ),
 [_NUM] = LAYOUT_kimiko_wrapper(
 // |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, XXXXXXX, KC_LPRN, KC_MINS, KC_RPRN, KC_COMM,                            KC_GRV,  _________NUM_456_________, KC_COLN, KC_DQUO,
     _______, KC_LSFT, KC_LBRC, KC_UNDS, KC_RBRC, KC_DOT, XXXXXXX,         XXXXXXX, XXXXXXX, _________NUM_123_________,   KC_QUES, KC_PIPE,
 // |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-                      KC_TRNS, KC_TRNS, _______, _______, _______,          _______, KC_0, _______, KC_LPRN, KC_RPRN
+                      KC_TRNS, KC_TRNS, _______, _______, _______,          _______, LT(_NAV, KC_0), _______, KC_LPRN, KC_RPRN
 ),
 
 [_NAV] = LAYOUT_kimiko_wrapper(
