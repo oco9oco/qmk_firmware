fa6bd61459816d078a6cd101b613ee75181b9bf4
Sungsoo Ryu <40929552+oco9oco@users.noreply.github.com>
2022-05-23 16:40:15 +0900

    LGUI_TO_THUMB

--- keyboards/keycapsss/kimiko/keymaps/oco9oco/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/oco9oco/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  __________________NUMROW_L_________________,                            __________________NUMROW_R_________________, KC_MINS,
     KC_GRAVE,__________________QWERTY_L1________________,                            __________________QWERTY_R1________________, KC_EQL,
     KC_CAPS, __________________QWERTY_SACS_L2___________,                            __________________QWERTY_HOME_R2___________, KC_QUOT,
-    KC_LSFT, __________________QWERTY_L3________________, TO_NOMOD,           KC_B,    __________________QWERTY_R3________________, KC_BSLS,
+    KC_LSFT, __________________QWERTY_L3________________, KC_LGUI,           KC_B,    __________________QWERTY_R3________________, KC_BSLS,
 // |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
                KC_LGUI,KC_HAEN,THUMB_L1,THUMB_L2,THUMB_L3,         THUMB_R3,THUMB_R2,THUMB_R1,KC_LBRC, KC_RBRC
 ),
