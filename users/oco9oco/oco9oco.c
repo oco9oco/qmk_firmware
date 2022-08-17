8146d05a4a65d0f1ea984d1de6a9f038b278f6f8
Sungsoo Ryu <40929552+oco9oco@users.noreply.github.com>
2022-08-17 09:39:12 +0900

    FIX

--- users/oco9oco/oco9oco.c
+++ users/oco9oco/oco9oco.c
@@ -243,6 +243,7 @@ case NAV(Z):
                 layer_move(_BASE);
             }
             break;
+
         // case SLSH_LL:
         //     if (__PRESSED__) {
         //         SEND_STRING(SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_SLSH) SS_TAP(X_RGHT) SS_TAP(X_RGHT));
