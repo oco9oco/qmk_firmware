49de19c4a0f26503830e12d024e6d005224120c2
Sungsoo Ryu <40929552+oco9oco@users.noreply.github.com>
2022-08-19 12:37:33 +0900

    oco9oco_autoshift

--- users/oco9oco/config.h
+++ users/oco9oco/config.h
@@ -12,12 +12,18 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
-#define TAPPING_FORCE_HOLD
+
+// TAP-HOLD OPTIONS
 #define TAPPING_TERM 200
+#define TAPPING_FORCE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
 #define PERMISSIVE_HOLD_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
-//MOUSEKEY
+
+// AUTO SHIFT OPTIONS
+#define NO_AUTO_SHIFT_ALPHA
+
+//MOUSEKEY OPTIONS
 
 #define MK_KINETIC_SPEED
 
@@ -41,9 +47,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define MOUSEKEY_WHEEL_TIME_TO_MAX 18
 #endif
 
+// ONESHOT OPTIONS
 #define ONESHOT_TAP_TOGGLE 1  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000
 
+// COMBO OPTIONS
 #define COMBO_TERM 40
 #define COMBO_TERM_PER_COMBO
 //#define COMBO_COUNT 4
