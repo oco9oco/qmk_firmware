e31e5153a9f5ab767629f796c40806005d7eb41d
oco9oco <oco9oco@gmail.com>
2022-09-23 15:31:18 +0900

    commit

--- keyboards/keycapsss/kimiko/keymaps/oco9oco/rules.mk
+++ keyboards/keycapsss/kimiko/keymaps/oco9oco/rules.mk
@@ -1,7 +1,7 @@
 OLED_ENABLE = no
 ENCODER_ENABLE = no       # ENables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = no
 AUTO_SHIFT_ENABLE= yes
-CAPS_WORD_ENABLE = yes
+CAPS_WORD_ENABLE = no
 LTO_ENABLE = yes
