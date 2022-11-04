62a03cf6fe9f1db63892b444e7fe0f2fe317d4a5
oco9oco <oco9oco@gmail.com>
2022-11-04 14:42:12 +0900

    fix

--- users/oco9oco/oco9oco.c
+++ users/oco9oco/oco9oco.c
@@ -194,6 +194,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
 // 괄호 커서이동 기능
         case KC_COMM:
+        case IPC_COMM:
             if (__PRESSED__/* && (brkt_pressed||prns_pressed)*/) {
                 cite_done=true;
             }return true;
