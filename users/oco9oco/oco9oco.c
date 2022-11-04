7f2d88ce5ac27b3eda6f0201e82c8b04727efd65
oco9oco <oco9oco@gmail.com>
2022-11-04 14:06:56 +0900

    comma

--- users/oco9oco/oco9oco.c
+++ users/oco9oco/oco9oco.c
@@ -161,7 +161,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
 // 괄호 커서이동 기능
         case KC_COMM:
-            if (__PRESSED__ && (brkt_pressed||prns_pressed)) {
+            if (__PRESSED__/* && (brkt_pressed||prns_pressed)*/) {
                 cite_done=true;
             }return true;
         case KC_1 ... KC_0:
